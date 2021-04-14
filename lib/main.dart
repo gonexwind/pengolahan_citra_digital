65d93a662f08e0427b674560da1da5e45e1a9ccf
gonexwind <fikky.ardi15@gmail.com>
2021-04-14 23:54:38 +0700

    add matrix color

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:pengolahan_citra_digital/constant.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Pengolahan Citra Digital',
+      theme: ThemeData(primarySwatch: Colors.teal),
       home: MyHomePage(),
     );
   }
@@ -56,8 +58,23 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text('Pengolahan Citra Digital'),
       ),
-      body: Center(
-        child: _image == null ? Text('No image selected.') : Image.file(_image),
+      body: ListView(
+        children: [
+          Center(
+              child: _image == null
+                  ? Text('No image selected.')
+                  : Image.file(_image)),
+          _buildButton(
+              text: 'Hitam Putih',
+              onPressed: () =>
+                  showAlertDialog(context, 'Hitam Putih', hitamPutih)),
+          _buildButton(
+              text: 'Ijo',
+              onPressed: () => showAlertDialog(context, 'Ijo', ijo)),
+          _buildButton(
+              text: 'Magenta',
+              onPressed: () => showAlertDialog(context, 'Magenta', magenta)),
+        ],
       ),
       floatingActionButton: SpeedDial(
         icon: Icons.add,
@@ -77,6 +94,40 @@ class _MyHomePageState extends State<MyHomePage> {
           )
         ],
       ),
+    );
+  }
+
+  Widget _buildButton({String text, Function onPressed}) {
+    return MaterialButton(
+      color: Colors.teal,
+      textColor: Colors.white,
+      child: Text(text),
+      onPressed: onPressed,
+    );
+  }
+
+  showAlertDialog(BuildContext context, String title, List<double> filter) {
+    Widget okButton = MaterialButton(
+      child: Text("OK"),
+      onPressed: () => Navigator.of(context).pop(),
+    );
+
+    AlertDialog alert = AlertDialog(
+      title: Text(title),
+      content: ColorFiltered(
+        colorFilter: ColorFilter.matrix(filter),
+        child: Image.file(_image),
+      ),
+      actions: [
+        okButton,
+      ],
+    );
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
     );
   }
 }
