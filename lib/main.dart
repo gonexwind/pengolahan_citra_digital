821cd21eb9cbbb11b82bf4e7b2883068f291b404
gonexwind <fikky.ardi15@gmail.com>
2021-04-15 00:32:48 +0700

    add bio

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:pengolahan_citra_digital/constant.dart';
@@ -28,6 +29,73 @@ class _MyHomePageState extends State<MyHomePage> {
   File _image;
   final picker = ImagePicker();
 
+  @override
+  Widget build(BuildContext context) {
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        statusBarColor: Colors.teal,
+        statusBarIconBrightness: Brightness.light,
+      ),
+      child: SafeArea(
+        child: Scaffold(
+          body: ListView(
+            children: [
+              _buildHeader(context),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  children: [
+                    Center(
+                        child: _image == null
+                            ? Text('No image selected.')
+                            : Image.file(_image)),
+                    SizedBox(height: 30),
+                    _buildButton(
+                        text: 'GreyScale',
+                        onPressed: () =>
+                            showAlertDialog(context, 'GreyScale', greyScale)),
+                    _buildButton(
+                        text: 'Lighten',
+                        onPressed: () =>
+                            showAlertDialog(context, 'Lighten', lighten)),
+                    _buildButton(
+                        text: 'Green',
+                        onPressed: () =>
+                            showAlertDialog(context, 'Green', green)),
+                    _buildButton(
+                        text: 'Magenta',
+                        onPressed: () =>
+                            showAlertDialog(context, 'Magenta', magenta)),
+                  ],
+                ),
+              ),
+              SizedBox(height: 50),
+            ],
+          ),
+          floatingActionButton: SpeedDial(
+            icon: Icons.add,
+            activeIcon: Icons.cancel,
+            visible: true,
+            closeManually: false,
+            children: [
+              SpeedDialChild(
+                child: Icon(Icons.camera_alt),
+                label: 'Ambil Gambar',
+                onTap: getImage,
+              ),
+              SpeedDialChild(
+                child: Icon(Icons.photo_library),
+                label: 'Pilih Foto',
+                onTap: getFile,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   Future getImage() async {
     final pickedFile = await picker.getImage(source: ImageSource.camera);
 
@@ -52,57 +120,15 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Pengolahan Citra Digital'),
-      ),
-      body: ListView(
-        children: [
-          Center(
-              child: _image == null
-                  ? Text('No image selected.')
-                  : Image.file(_image)),
-          _buildButton(
-              text: 'Hitam Putih',
-              onPressed: () =>
-                  showAlertDialog(context, 'Hitam Putih', hitamPutih)),
-          _buildButton(
-              text: 'Ijo',
-              onPressed: () => showAlertDialog(context, 'Ijo', ijo)),
-          _buildButton(
-              text: 'Magenta',
-              onPressed: () => showAlertDialog(context, 'Magenta', magenta)),
-        ],
-      ),
-      floatingActionButton: SpeedDial(
-        icon: Icons.add,
-        activeIcon: Icons.cancel,
-        visible: true,
-        closeManually: false,
-        children: [
-          SpeedDialChild(
-            child: Icon(Icons.camera_alt),
-            label: 'Ambil Gambar',
-            onTap: getImage,
-          ),
-          SpeedDialChild(
-            child: Icon(Icons.photo_library),
-            label: 'Pilih Foto',
-            onTap: getFile,
-          )
-        ],
-      ),
-    );
-  }
-
   Widget _buildButton({String text, Function onPressed}) {
-    return MaterialButton(
-      color: Colors.teal,
-      textColor: Colors.white,
-      child: Text(text),
-      onPressed: onPressed,
+    return Container(
+      width: double.infinity,
+      child: MaterialButton(
+        color: Colors.teal,
+        textColor: Colors.white,
+        child: Text(text),
+        onPressed: onPressed,
+      ),
     );
   }
 
@@ -128,6 +154,58 @@ class _MyHomePageState extends State<MyHomePage> {
       builder: (BuildContext context) {
         return alert;
       },
+    );
+  }
+
+  Widget _buildHeader(BuildContext context) {
+    return Container(
+      height: (MediaQuery.of(context).orientation == Orientation.landscape)
+          ? 100
+          : 120,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.teal,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(50),
+          bottomRight: Radius.circular(50),
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 10),
+          Text(
+            'Pengolahan Citra Digital',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 22,
+            ),
+          ),
+          Text(
+            'Fikky Ardianto',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+            ),
+          ),
+          Text(
+            '201851136',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+            ),
+          ),
+          Text(
+            'Kelas Y',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(height: 10),
+        ],
+      ),
     );
   }
 }
